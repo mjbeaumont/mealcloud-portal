d588be926d33311d7d04660c898027a6cb4491b2
Matthew Beaumont <matt@beaumontwebdev.com>
2020-06-19 11:00:42 -0400

    fixes to order list page

--- views/order/_order-list.php
+++ views/order/_order-list.php
@@ -2,9 +2,12 @@
 
 /***
  * @var $orders array
+ * @var $text_color string
  */
 
 use yii\helpers\Url;
+
+$item_class = isset($text_color) ? 'text-' . $text_color : '';
 ?>
 
 <?php if (count($orders)):?>
@@ -12,7 +15,7 @@ use yii\helpers\Url;
 <div class="list-group">
 	<?php foreach ($orders as $order):?>
 	<a href="<?php echo Url::to(['/order/detail', 'id' => $order->id])?>" class="list-group-item">
-        <div class="order-item">
+        <div class="order-item <?php echo $item_class?>">
 			<div><?php echo $order->TypeDescription?></div>
 			<div><?php echo $order->name?><br> <?php echo $order->CountItems?> Items for <?php echo $order->number?></div>
 			<div><?php echo \Yii::$app->formatter->asCurrency($order->total)?></div>
--- views/order/index.php
+++ views/order/index.php
@@ -9,54 +9,52 @@
 use yii\helpers\Html;
 ?>
 
-<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+<h4>Status: <span class="text-success">Online and accepting orders</span></h4>
+<div class="panel-group" role="tablist" aria-multiselectable="false">
     <div class="panel panel-default">
         <div class="panel-heading" role="tab" id="headingOne">
             <h4 class="panel-title">
-                <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne"
-                   aria-expanded="false" aria-controls="collapseOne">
+                <a role="button" data-toggle="collapse" href="#collapseOne"
+                   aria-expanded="true" aria-controls="collapseOne">
                     New Orders (<?php echo count($new)?>)
                 </a>
             </h4>
         </div>
         <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
             <?php echo $this->render("_order-list", [
-                   'orders' => $new
+                   'orders' => $new,
+                   'text_color' => 'danger'
             ])?>
         </div>
     </div>
     <div class="panel panel-default">
         <div class="panel-heading" role="tab" id="headingTwo">
             <h4 class="panel-title">
-                <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo"
-                   aria-expanded="false" aria-controls="collapseTwo">
+                <a role="button" data-toggle="collapse" href="#collapseTwo"
+                   aria-expanded="true" aria-controls="collapseTwo">
                     In Progress (<?php echo count($inProgress)?>)
                 </a>
             </h4>
         </div>
-        <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
-            <div class="panel-body">
-	            <?php echo $this->render("_order-list", [
-		            'orders' => $inProgress
-	            ])?>
-            </div>
+        <div id="collapseTwo" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingTwo">
+            <?php echo $this->render("_order-list", [
+		        'orders' => $inProgress
+	        ])?>
         </div>
     </div>
     <div class="panel panel-default">
         <div class="panel-heading" role="tab" id="headingThree">
             <h4 class="panel-title">
-                <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree"
+                <a role="button" data-toggle="collapse" href="#collapseThree"
                    aria-expanded="false" aria-controls="collapseThree">
                     Completed (<?php echo count($completed)?>)
                 </a>
             </h4>
         </div>
         <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
-            <div class="panel-body">
-	            <?php echo $this->render("_order-list", [
-		            'orders' => $completed
-	            ])?>
-            </div>
+	        <?php echo $this->render("_order-list", [
+		        'orders' => $completed
+	        ])?>
         </div>
     </div>
 </div>
